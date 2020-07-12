a9e6a341bdc0af9758db7ebce5d3ff7b79b6a4a4
sriram161 <k.sriramsagar@gmail.com>
2020-07-12 15:38:20 -0400

    poetry package description.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+$sudo apt install libopenblas-dev libblas-dev m4 cmake cython python3-dev python3-yaml python3-setuptools
+$cd wheels
+$pip3 install torch-1.1.0-cp37-cp37m-linux_armv6l.whl
+$pip3 install torchvision-0.3.0-cp37-cp37m-linux_armv6l.whl
