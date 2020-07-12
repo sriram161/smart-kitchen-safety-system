86a40222bb35ec8ffd7c2a90a32eb50db0ea4361
sriram161 <k.sriramsagar@gmail.com>
2020-07-12 16:24:41 -0400

    setup_wheels udpated.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 sudo apt install libopenblas-dev libblas-dev m4 cmake cython python3-dev python3-yaml python3-setuptools
 poetry config virtualenvs.in-project true
 poetry shell
-cd wheels
+cd setup_wheels
 pip3 install torch-1.1.0-cp37-cp37m-linux_armv6l.whl
 pip3 install torchvision-0.3.0-cp37-cp37m-linux_armv6l.whl
 cd ..
+# scp *.whl pi@raspberrypi:/home/pi/Documents/smart-kitchen-safety-system/setup_wheels
